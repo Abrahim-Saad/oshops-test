8a3c9a266f4f13d8768a9edb31cb91fda2912a58
Ibrahim Saad Ibrahim Ali <abrahimsaad271@gmail.com>
2021-12-13 15:12:09 +0200

    Update users.routes.js

--- modules/users/routes/users.routes.js
+++ modules/users/routes/users.routes.js
@@ -1,9 +1,9 @@
 const app = require("express").Router();
-const { signUp, userSignIn, editUserProfile, resetPassword, addAddress, removeAddress, getAllUsers, getCurrentUser} = require('../controller/users.controller');
+const { signUp, userSignIn, editUserProfile, resetPassword, addAddress, removeAddress, getAllUsers, getCurrentUser, sendOrder} = require('../controller/users.controller');
 const validator = require('../../../validation/common.validation');
-const { userSignInValidation, editUserProfileValidation, signUpValidation, resetPasswordValidation, addAddressValidation} = require("../validation/users.validation");
+const { userSignInValidation, editUserProfileValidation, signUpValidation, resetPasswordValidation, addAddressValidation, sendOrderValidation} = require("../validation/users.validation");
 const isAuthorized = require("../../../config/isAuthorized");
-const { Edit_User_PROFILE, RESET_PASSWORD, ADD_ADDRESS, REMOVE_ADDRESS, GET_ALL_USERS, GET_CURRENT_USER } = require("../../../endPoints/endPoints");
+const { Edit_User_PROFILE, RESET_PASSWORD, ADD_ADDRESS, REMOVE_ADDRESS, GET_ALL_USERS, GET_CURRENT_USER, SEND_ORDER } = require("../../../endPoints/endPoints");
 
 app.post("/signUp", [validator(signUpValidation)], signUp);
 app.post("/userSignIn", [validator(userSignInValidation)], userSignIn);
@@ -13,5 +13,6 @@ app.put("/addAddress", [isAuthorized(ADD_ADDRESS), validator(addAddressValidatio
 app.put("/removeAddress", [isAuthorized(REMOVE_ADDRESS)], removeAddress);
 app.get("/getAllUsers", [isAuthorized(GET_ALL_USERS)], getAllUsers);
 app.get("/getCurrentUser", [isAuthorized(GET_CURRENT_USER)], getCurrentUser);
- 
+app.post("/sendOrder", [isAuthorized(SEND_ORDER), validator(sendOrderValidation)], sendOrder);
+
 module.exports = app;
