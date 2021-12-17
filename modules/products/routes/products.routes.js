12c71771485a31a6a55ab0e1a2d0ea5ea54b4c99
Ibrahim Saad Ibrahim Ali <abrahimsaad271@gmail.com>
2021-12-17 17:24:21 +0200

    Update products.routes.js

--- modules/products/routes/products.routes.js
+++ modules/products/routes/products.routes.js
@@ -12,13 +12,13 @@ const { ADD_PRODUCT, UPDATE_PRODUCT, DELETE_PRODUCT, GET_ALL_PRODUCT, GET_PRODUC
 app.post("/addProduct", [isAuthorized(ADD_PRODUCT),validator(addProductValidation)], addProduct);
 app.put("/updateProduct/:id", [isAuthorized(UPDATE_PRODUCT), validator(updateProductValidation)], updateProduct);
 app.delete("/deleteProduct/:id", [isAuthorized(DELETE_PRODUCT)], deleteProduct);
-app.get("/getAllProducts", [isAuthorized(GET_ALL_PRODUCT)], getAllProducts);
-app.get("/getProductById/:id", [isAuthorized(GET_PRODUCT_BY_ID)], getProductById);
+app.get("/getAllProducts", getAllProducts);
+app.get("/getProductById/:id", getProductById);
 
-app.get("/topRatedProducts", [isAuthorized(TOP_RATED_PRODUCTS)], topRatedProducts);
-app.get("/storeTopRatedProducts/:storeId", [isAuthorized(STORE_TOP_RATED_PRODUCTS)], storeTopRatedProducts);
-app.get("/storeProducts/:storeId", [isAuthorized(STORE_PRODUCTS)], storeProducts);
-app.get("/productsByStoreAndCategory/:storeId/:categoryId", [isAuthorized(PRODUCT_BY_STORE_AND_CATEGORY)], productsByStoreAndCategory);
-app.get("/productSearch", [isAuthorized(PRODUCT_SEARCH)], productSearch);
+app.get("/topRatedProducts", topRatedProducts);
+app.get("/storeTopRatedProducts/:storeId", storeTopRatedProducts);
+app.get("/storeProducts/:storeId", storeProducts);
+app.get("/productsByStoreAndCategory/:storeId/:categoryId", productsByStoreAndCategory);
+app.get("/productSearch", productSearch);
 
 module.exports = app;
