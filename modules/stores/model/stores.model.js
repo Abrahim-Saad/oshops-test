83ec686d21ddffb75b2b0c46ae3a733210113181
Ibrahim Saad Ibrahim Ali <abrahimsaad271@gmail.com>
2022-04-08 01:18:13 +0200

    delete

--- modules/stores/model/stores.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const storeSchema = require('../schema/stores.schema');
-
-const storeModel = mongoose.model('store', storeSchema);
-
-module.exports = storeModel;
